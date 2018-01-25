2acb43ffec1a72f35dd2a6f12c484a59b7f52b0b
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-25 16:09:19 +0100

    Tests created

---
+++ src/test/crossover/CrossoverTest.java
@@ -0,0 +1,66 @@
+package test.crossover;
+
+import base.City;
+import base.Tour;
+import crossover.ICrossover;
+import data.InstanceReader;
+import data.TSPLIBReader;
+import main.Configuration;
+import org.junit.Assert;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public abstract class CrossoverTest {
+
+    protected Tour generateTestTour() {
+        Tour tour = new Tour();
+        ArrayList<City> cities = new ArrayList<>(availableCities);
+        Collections.shuffle(cities);
+        tour.setCities(cities);
+        return tour;
+    }
+
+    protected CrossoverTest() {
+        InstanceReader instanceReader = new InstanceReader(Configuration.instance.dataFilePath);
+        instanceReader.open();
+        TSPLIBReader tspLibReader = new TSPLIBReader(instanceReader);
+
+        availableCities = tspLibReader.getCities();
+    }
+
+    static ArrayList<City> availableCities;
+
+    /**
+     * checks whether the created tour has the same amount of cities as the parents
+     *
+     * @param crossover CrossOver instance to use
+     * @param parent1   parent used for generation
+     * @param parent2   parent used for generation
+     */
+    public void checkSize(ICrossover crossover, Tour parent1, Tour parent2) {
+
+        Tour generatedTour = crossover.doCrossover(parent1, parent2);
+
+        Assert.assertEquals(parent1.getSize(), parent2.getSize());
+        Assert.assertEquals(parent1.getSize(), generatedTour.getSize());
+    }
+
+    /**
+     * checks whether all cities of the parents are inside the generated tour
+     *
+     * @param crossover CrossOver instance to use
+     * @param parent1   parent used for generation
+     * @param parent2   parent used for generation
+     */
+    public void checkAllCityStillContained(ICrossover crossover, Tour parent1, Tour parent2) {
+        Tour generatedTour = crossover.doCrossover(parent1, parent2);
+
+        //test if all cities of initial tour 1 are in the generated tour.
+        for (int i = 0; i < parent1.getSize(); i++) {
+            City currentCity = parent1.getCity(i);
+            Assert.assertTrue("City not in generated tour: " + currentCity.toString(), generatedTour.containsCity(currentCity));
+        }
+    }
+}
--- src/test/crossover/CycleCrossover.java
+++ src/test/crossover/CycleCrossover.java
@@ -1,4 +1,28 @@
 package test.crossover;
 
-public class CycleCrossover {
+import base.City;
+import base.Tour;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CycleCrossover extends CrossoverTest {
+
+    public CycleCrossover() {
+        super();
+    }
+
+    @Test
+    public void testSize() {
+        crossover.CycleCrossover cc = new crossover.CycleCrossover();
+        // TODO: 25.01.2018  insert tour here
+        checkSize(cc, generateTestTour(), generateTestTour());
+    }
+
+    @Test
+    public void testAllCityStillContained() {
+        crossover.CycleCrossover cc = new crossover.CycleCrossover();
+        // TODO: 25.01.2018  insert tour here
+        checkAllCityStillContained(cc, generateTestTour(), generateTestTour());
+    }
+
 }
