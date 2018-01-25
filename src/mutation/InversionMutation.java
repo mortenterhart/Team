6f9e2c0802a81e11a027da21339fc9efb2822282
FlorianEder <florian.eder97@gmail.com>
2018-01-25 15:51:45 +0100

    Implemented InversionMotation

--- src/mutation/InversionMutation.java
+++ src/mutation/InversionMutation.java
@@ -1,10 +1,45 @@
 package mutation;
 
+import base.City;
 import base.Tour;
+import random.MersenneTwisterFast;
+
+import java.util.ArrayList;
 
 public class InversionMutation implements IMutation {
     public Tour doMutation(Tour tour) {
-        return null;
+        MersenneTwisterFast mtf = new MersenneTwisterFast();
+
+        ArrayList<City> cities = tour.getCities();
+
+        int point1 = mtf.nextInt(0, cities.size()-1);
+        int point2 = mtf.nextInt(0, cities.size()-1);
+
+        int startPoint;
+        int endPoint;
+
+        if(point1<point2)
+        {
+            startPoint = point1;
+            endPoint = point2;
+        }else
+        {
+            startPoint = point2;
+            endPoint = point1;
+        }
+        System.out.println("StartPoint: " + startPoint);
+        System.out.println("EndPoint: " + endPoint);
+
+        City tempCity = null;
+
+        for(int i = 0; i<(endPoint-startPoint)/2; i++)
+        {
+            tempCity = cities.get(startPoint+i);
+            cities.set(startPoint+i, cities.get(endPoint-i));
+            cities.set(endPoint-i, tempCity);
+        }
+
+        return tour;
     }
 
     public String toString() {
--- src/test/mutation/InversionMutation.java
+++ src/test/mutation/InversionMutation.java
@@ -1,4 +1,37 @@
 package test.mutation;
 
+import base.City;
+import base.Tour;
+import mutation.IMutation;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class InversionMutation {
+
+    @Test
+    public void testDoMutation()
+    {
+        Tour tour = new Tour();
+        ArrayList<City> oldCities = new ArrayList<City>();
+        ArrayList<City> cities = new ArrayList<City>();
+        for(int i = 0; i<10; i++){
+            oldCities.add(new City(i,i,i));
+            cities.add(new City(i,i,i));
+        }
+
+        tour.setCities(cities);
+
+        IMutation inversionMutation = new mutation.InversionMutation();
+
+        tour = inversionMutation.doMutation(tour);
+
+        for(City c : tour.getCities())
+        {
+            System.out.println(c.toString());
+        }
+
+    }
+
+
 }
