fcf4c6fbf20d39c708c3dc54082a78823c2beb17
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-25 17:23:47 +0100

    Duplicate check added to city concurrency test, due to random data generation.

--- src/test/crossover/CrossoverTest.java
+++ src/test/crossover/CrossoverTest.java
@@ -48,7 +48,7 @@ public abstract class CrossoverTest {
     }
 
     /**
-     * checks whether all cities of the parents are inside the generated tour
+     * checks whether all cities of the parents are inside the generated tour, checks for duplicates
      *
      * @param crossover CrossOver instance to use
      * @param parent1   parent used for generation
@@ -57,6 +57,10 @@ public abstract class CrossoverTest {
     public void checkAllCityStillContained(ICrossover crossover, Tour parent1, Tour parent2) {
         Tour generatedTour = crossover.doCrossover(parent1, parent2);
 
+        //since test data is generated randomly,
+        // we need to check for duplicates by comparing the size of the generated tour
+        Assert.assertEquals(parent1.getSize(), parent2.getSize());
+        Assert.assertEquals(parent1.getSize(), generatedTour.getSize());
         //test if all cities of initial tour 1 are in the generated tour.
         for (int i = 0; i < parent1.getSize(); i++) {
             City currentCity = parent1.getCity(i);
