1b2f9e48a8a8fbc025e841683c11cd3c603605dd
Paddy3108 <Patrick.Eichert@web.de>
2018-01-25 15:11:52 +0100

    Implementation for DisplacementMutation and test.

--- src/mutation/DisplacementMutation.java
+++ src/mutation/DisplacementMutation.java
@@ -1,10 +1,51 @@
 package mutation;
 
+import base.City;
 import base.Tour;
+import random.MersenneTwisterFast;
+
+import java.util.ArrayList;
 
 public class DisplacementMutation implements IMutation {
     public Tour doMutation(Tour tour) {
-        return null;
+        MersenneTwisterFast twisterFast = new MersenneTwisterFast();
+        //get start index
+        int startIndex = twisterFast.nextInt(0, tour.getCities().size()-1);
+        //get end index
+        int endIndex = twisterFast.nextInt(0, tour.getCities().size()-1);
+        while (endIndex == startIndex)
+            endIndex = twisterFast.nextInt(0, tour.getCities().size()-1);
+
+        //swap startIndex and endIndex if endIndex is smaller
+        if (startIndex > endIndex) {
+            int swap = startIndex;
+            startIndex = endIndex;
+            endIndex = swap;
+        }
+        ArrayList<City> finalCities = tour.getCities();
+
+        //TODO rename
+        ArrayList<City> helpCities = new ArrayList<>();
+
+        int difference = endIndex-startIndex;
+        for (int i = 0; i <= difference; i++)
+        {
+            helpCities.add(finalCities.get(startIndex));
+            finalCities.remove(startIndex);
+        }
+
+        //get random insertPoint
+        int insertPoint = twisterFast.nextInt(0, tour.getCities().size()-1);
+        while (insertPoint==startIndex)
+            insertPoint = twisterFast.nextInt(0, tour.getCities().size()-1);
+
+        for (int i = 0; i < helpCities.size() ; i++)
+        {
+            finalCities.add(insertPoint+i, helpCities.get(i));
+        }
+
+        tour.setCities(finalCities);
+        return tour;
     }
 
     public String toString() {
--- src/test/mutation/DisplacementMutation.java
+++ src/test/mutation/DisplacementMutation.java
@@ -1,4 +1,49 @@
 package test.mutation;
 
+import base.City;
+import base.Tour;
+import mutation.IMutation;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class DisplacementMutation {
+
+    @Test
+    public void testDisplacementMutation(){
+        Tour tour = new Tour();
+        ArrayList<City> oldCities = new ArrayList<City>();
+        ArrayList<City> cities = new ArrayList<City>();
+        for(int i = 0; i<10; i++){
+            oldCities.add(new City(i,i,i));
+            cities.add(new City(i,i,i));
+        }
+
+        tour.setCities(cities);
+        IMutation displacementMutation = new mutation.DisplacementMutation();
+
+        //start test and get response
+        tour = displacementMutation.doMutation(tour);
+        ArrayList<City> newCities = tour.getCities();
+
+        //count ids to check if all cities are contained
+        int sum = 0;
+        for(City c : oldCities)
+            sum = sum + c.getId();
+
+        Assert.assertEquals(45, sum );
+
+        //check if size is equal
+        Assert.assertEquals(newCities.size(), oldCities.size());
+
+        //check if sequence is equal
+        Boolean isEqual = true;
+        for(int i=0; i<newCities.size(); i++)
+            if(newCities.get(i).getX() != oldCities.get(i).getX())
+                isEqual = false;
+
+        Assert.assertEquals(false, isEqual);
+
+    }
 }
