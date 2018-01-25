a669ffe4946d3524bcfb2ede0b0051bb53558b5b
Paddy3108 <Patrick.Eichert@web.de>
2018-01-25 15:23:41 +0100

    Implementation for ExchangeMutation and test.

--- src/test/mutation/ExchangeMutation.java
+++ src/test/mutation/ExchangeMutation.java
@@ -1,4 +1,48 @@
 package test.mutation;
 
+import base.City;
+import base.Tour;
+import mutation.IMutation;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ExchangeMutation {
+    @Test
+    public void testExchangeMutation(){
+        Tour tour = new Tour();
+        ArrayList<City> oldCities = new ArrayList<City>();
+        ArrayList<City> cities = new ArrayList<City>();
+        for(int i = 0; i<10; i++){
+            oldCities.add(new City(i,i,i));
+            cities.add(new City(i,i,i));
+        }
+
+        tour.setCities(cities);
+        IMutation exchangeMutation = new mutation.ExchangeMutation();
+
+        //start test and get response
+        tour = exchangeMutation.doMutation(tour);
+        ArrayList<City> newCities = tour.getCities();
+
+        //count ids to check if all cities are contained
+        int sum = 0;
+        for(City c : oldCities)
+            sum = sum + c.getId();
+
+        Assert.assertEquals(45, sum );
+
+        //check if size is equal
+        Assert.assertEquals(newCities.size(), oldCities.size());
+
+        //check if sequence is equal
+        Boolean isEqual = true;
+        for(int i=0; i<newCities.size(); i++)
+            if(newCities.get(i).getX() != oldCities.get(i).getX())
+                isEqual = false;
+
+        Assert.assertEquals(false, isEqual);
+        
+    }
 }
