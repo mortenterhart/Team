6e495a6ebc85e1bdbe9d4bf415f8a1e7c5c16b4f
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-26 11:23:42 +0100

    Test class refactored

---
+++ src/test/crossover/TestTourGenerator.java
@@ -0,0 +1,28 @@
+package test.crossover;
+
+import base.City;
+import base.Tour;
+import main.Configuration;
+import random.MersenneTwisterFast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestTourGenerator {
+    public Tour generateTourAllCities() {
+        return generateTour(CityTestData.Data.getAllCities());
+    }
+
+    public Tour generateTourTestCities() {
+        return generateTour(CityTestData.Data.getTestCities());
+    }
+
+    private Tour generateTour(ArrayList<City> cities) {
+        Tour tour = new Tour();
+        Collections.shuffle(cities, Configuration.instance.mersenneTwister);
+        tour.setCities(cities);
+        return tour;
+    }
+
+}
