6f7cb685c3ec5e8ffcd92b3a65c375a6917551e7
Paddy3108 <Patrick.Eichert@web.de>
2018-01-26 10:27:58 +0100

    write heuristicMutation test

--- src/test/mutation/HeuristicMutation.java
+++ src/test/mutation/HeuristicMutation.java
@@ -3,48 +3,38 @@ package test.mutation;
 import base.City;
 import base.Tour;
 import mutation.IMutation;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class HeuristicMutation {
+    private  Tour tour;
+
+    @Before
+    public void initTour(){
+        this.tour = new Tour();
+        ArrayList<City> cities = new ArrayList<City>();
+        for(int i = 0; i<20; i++){
+            cities.add(new City(i,i,i));
+        }
+        this.tour.setCities(cities);
+    }
 
     @Test
-    public void testHeuristicMutation() {
-        Tour tour = new Tour();
-        ArrayList<City> oldCities = new ArrayList<City>();
-        ArrayList<City> cities = new ArrayList<City>();
-        for (int i = 0; i < 10; i++) {
-            oldCities.add(new City(i, i, i));
-            cities.add(new City(i, i, i));
-        }
-
-        tour.setCities(cities);
+    public void checkSize(){
         IMutation heuristicMutation = new mutation.HeuristicMutation();
+        this.tour = heuristicMutation.doMutation(this.tour);
+        assertEquals(20, tour.getCities().size());
+    }
 
-        //start test and get response
-        tour = heuristicMutation.doMutation(tour);
-
-       /* ArrayList<City> newCities = tour.getCities();
-
-        //count ids to check if all cities are contained
-        int sum = 0;
-        for(City c : newCities)
-            sum = sum + c.getId();
-
-        Assert.assertEquals(45, sum );
-
-        //check if size is equal
-        Assert.assertEquals(newCities.size(), oldCities.size());
-
-        //check if sequence is equal
-        Boolean isEqual = true;
-        for(int i=0; i<newCities.size(); i++)
-            if(newCities.get(i).getX() != oldCities.get(i).getX())
-                isEqual = false;
-
-        Assert.assertEquals(false, isEqual);*/
-
+    @Test
+    public void checkNotNull(){
+        IMutation heuristicMutation = new mutation.HeuristicMutation();
+        this.tour = heuristicMutation.doMutation(this.tour);
+        assertNotNull(this.tour.getCities());
     }
 }
