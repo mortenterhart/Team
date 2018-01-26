332a22fa7fa8ad4e4fd4bfb0568387ded93b9065
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-26 12:10:34 +0100

    fix ordered crossover

--- src/crossover/OrderedCrossover.java
+++ src/crossover/OrderedCrossover.java
@@ -9,38 +9,44 @@ public class OrderedCrossover implements ICrossover {
         int size = tour01.getSize();
 
         //choose start and end indices of slice
-        int rand1 = Configuration.instance.mersenneTwister.nextInt(0, size - 2);
-        int rand2 = Configuration.instance.mersenneTwister.nextInt(0, size - 1);
+        int rand1 = Configuration.instance.mersenneTwister.nextInt(0, size - 1);
+        int rand2 = Configuration.instance.mersenneTwister.nextInt(rand1, size - 2);
 
         int start = Math.min(rand1, rand2);
         int end = Math.max(rand1, rand2);
 
         Tour child1 = new Tour(size);
         Tour child2 = new Tour(size);
-
         //add slice from parents to children
         for(int i = start; i < end; i++) {
             child1.addCity(i, tour01.getCity(i));
             child2.addCity(i, tour02.getCity(i));
         }
 
-        int toSaveIdx = 0;
+        int toSaveIdx1 = 0, toSaveIdx2 = 0;
         //loop through all parent elements
         for (int i = 0; i < size; i++) {
-            if(toSaveIdx == start) { //check if we need to skip already copied slice
-                toSaveIdx = end;
+            if(toSaveIdx1 == start) { //check if we need to skip already copied slice
+                toSaveIdx1 = end;
+            }
+            if(toSaveIdx2 == start) {
+                toSaveIdx2 = end;
             }
 
             City c1 = tour01.getCity(i);
             City c2 = tour02.getCity(i);
 
+
             //add missing cities to empty spots of array
             if(!child1.containsCity(c2)) {
-                child1.addCity(toSaveIdx, c2);
+                child1.addCity(toSaveIdx1, c2);
+                toSaveIdx1++;
             }
             if(!child2.containsCity(c1)) {
-                child2.addCity(toSaveIdx, c1);
+                child2.addCity(toSaveIdx2, c1);
+                toSaveIdx2++;
             }
+
         }
 
         //return fittest child
